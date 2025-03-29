f1fb2be0b95522c65ba916652f7ba7e2b54752a7
Diego <diegofari02@gmail.com>
2025-03-29 13:22:35 +0100

    fixedFinalQ

--- ExpcodeG1.js
+++ ExpcodeG1.js
@@ -4,19 +4,38 @@ PennController.ResetPrefix(null); // Shorten command names (keep this line here)
 
 // Show the consent first, then intro page with instructions
 // then all the 'experiment' trials in a random order, then send the results and finally show the trial labeled 'end'
-Sequence("consent", "intro", "trial", "trial_end", "experiment", "ManipulationCheck", "MCDSQuestionaire", SendResults(), "end");
+Sequence(
+  "consent",
+  "intro",
+  "trial",
+  "trial_end",
+  "experiment",
+  "ManipulationCheck",
+  "MCDSQuestionaire",
+  SendResults(),
+  "end"
+);
 
 // Showing consent, stored in a html file that you can edit
 newTrial(
   "consent",
   defaultText.print().center(),
   newHtml("consent", "consent.html").print().center(),
-  newText("instructions", "Press the spacebar to give consent to the experiment.").print().center(),
+  newText(
+    "instructions",
+    "Press the spacebar to give consent to the experiment."
+  )
+    .print()
+    .center(),
   newKey("spacebar", " ").wait()
 );
 
 // Showing page with instructions, in a html file that you can edit
-newTrial("intro", newHtml("intro.html").print(), newButton("Continue.").center().print().wait());
+newTrial(
+  "intro",
+  newHtml("intro.html").print(),
+  newButton("Continue.").center().print().wait()
+);
 
 // Starting with the trial section
 Template("trialitems.csv", (row) =>
@@ -24,7 +43,18 @@ Template("trialitems.csv", (row) =>
     "trial",
 
     newText("question", row.question).center().print(),
-    newScale("response", row.answer1, row.answer2, row.answer3, row.answer4, row.answer5).center().labelsPosition("top").size("auto").print(), // Adding the scale to answer
+    newScale(
+      "response",
+      row.answer1,
+      row.answer2,
+      row.answer3,
+      row.answer4,
+      row.answer5
+    )
+      .center()
+      .labelsPosition("top")
+      .size("auto")
+      .print(), // Adding the scale to answer
     newButton("Next").center().print().wait()
   )
 );
@@ -49,7 +79,19 @@ Template("expitems.csv", (row) =>
     newMouseTracker("mouse") // Starting the mouse tracking
       .log()
       .start(),
-    newScale("response", row.answer1, row.answer2, row.answer3, row.answer4, row.answer5).center().labelsPosition("top").size("auto").print().log(), // Adding the scale to answer
+    newScale(
+      "response",
+      row.answer1,
+      row.answer2,
+      row.answer3,
+      row.answer4,
+      row.answer5
+    )
+      .center()
+      .labelsPosition("top")
+      .size("auto")
+      .print()
+      .log(), // Adding the scale to answer
     newButton("Next").center().print().wait(),
     // Stop RT
     getTimer("RT").stop(),
@@ -63,8 +105,17 @@ Template("expitems.csv", (row) =>
 newTrial(
   "ManipulationCheck",
 
-  newText("I felt that my answers are going to be kept anonymous").center().print(),
-  newScale("response", "Strongly disagree", "Disagree", "Not agree nor disagree", "Agree", "Strongly agree")
+  newText("I felt that my answers are going to be kept anonymous")
+    .center()
+    .print(),
+  newScale(
+    "response",
+    "Strongly disagree",
+    "Disagree",
+    "Not agree nor disagree",
+    "Agree",
+    "Strongly agree"
+  )
     .center()
     .labelsPosition("top")
     .size("auto")
@@ -73,12 +124,17 @@ newTrial(
   newButton("Next").center().print().wait()
 );
 
-Template("MCDSQuestionaire", (row) =>
+Template("FinalQ.csv", (row) =>
   newTrial(
-    "MCDSQ",
+    "MCDSQuestionaire",
 
     newText("question", row.question).center().print(),
-    newScale("response", "True", "False").center().labelsPosition("top").log().size("auto").print(), // Adding the scale to answer
+    newScale("response", "True", "False")
+      .center()
+      .labelsPosition("top")
+      .log()
+      .size("auto")
+      .print(), // Adding the scale to answer
     newButton("Next").center().print().wait()
   )
 );
@@ -89,7 +145,11 @@ newTrial(
   exitFullscreen(),
   newText("Thank you for your participation!").center().print(),
   // This is a dummy link, it won't actually validate submissions; use the link provided by your pooling platform
-  newText("<p><a href='https://www.pcibex.net/' target='_blank'>Click here to validate your submission</a></p>").center().print(),
+  newText(
+    "<p><a href='https://www.pcibex.net/' target='_blank'>Click here to validate your submission</a></p>"
+  )
+    .center()
+    .print(),
   // Wait on this page forever
   newButton().wait()
 ).setOption("countsForProgressBar", false);
